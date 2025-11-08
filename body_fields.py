ed1db5b3b5912a565abc863d4f98cfd6439ce77f
Sadot Arefin <sadot.arefin@gmail.com>
2025-11-09 01:40:22 +0600

    added support for dict and list of models

--- body_fields.py
+++ body_fields.py
@@ -2,12 +2,12 @@
 from typing import Annotated
 
 from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
 
 app = FastAPI()
 
 class Image(BaseModel):
-    url: str
+    url: HttpUrl
     name: str
 
 class Item(BaseModel):
@@ -20,8 +20,27 @@ class Item(BaseModel):
     #tags: list[str] = []
     tags: set[str] = set() #as set
     image: Image | None = None
+    thumbs: list[Image] | None = None
+
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[Item]
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
     results = {"item_id": item_id, "item": item}
     return results
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer
+
+@app.post("/images/multiple/")
+async def create_multiple_images(images: list[Image]):
+    return images
+
+@app.post("/index-weights/")
+async def create_index_weights(weights: dict[int, float]):
+    return weights
