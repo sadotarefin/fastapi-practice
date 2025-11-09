b3f951354f72253745527b6bf95b1f61f9986357
Sadot Arefin <sadot.arefin@gmail.com>
2025-11-09 22:46:50 +0600

    added basic scheme example

--- qpm.py
+++ qpm.py
@@ -16,6 +16,28 @@ class FilterParams(BaseModel):
     order_by: Literal[MyOrder.CREATED_AT, MyOrder.UPDATED_AT] = MyOrder.CREATED_AT
     tags: list[str] = []
 
+class Item(BaseModel):
+    name: str
+    description: str | None = Field(default=None, examples=["A very good example"]) #this will work in case no model_config
+    price: float
+    tax: float | None = None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples":[{
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 2.3
+            }
+            ]
+        }
+    }
+
 @app.get("/items/")
 async def read_items(filter_query: Annotated[FilterParams, Query()]):
     return filter_query
+
+@app.post("/items/")
+async def create_items(item: Item):
+    return item
