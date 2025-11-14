d5219d4b25caef3394af4b236ff48e58b5c35a33
Sadot Arefin <sadot.arefin@gmail.com>
2025-11-15 01:18:37 +0600

    dependencies - added yield

---
+++ dependencies_with_yield.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+from fastapi import FastAPI, Depends, HTTPException, status
+
+app = FastAPI()
+
+class InternalError(Exception):
+    pass
+
+def get_username():
+    try:
+        yield "Rick" #simulate getting current username
+    except InternalError:
+        print("We do not shallow the interal exception here, we raise them")
+        raise
+    finally:
+        print("cleaning up")
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, username: Annotated[str, Depends(get_username)]):
+    if item_id == 'portal-gun':
+        raise InternalError(f"Portal gun cannot be owned by {username}")
+    if item_id != 'foo':
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "item not found"
+        )
+    print("Sending response back")
+    return item_id
+
